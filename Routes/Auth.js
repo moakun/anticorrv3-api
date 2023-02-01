54307da39045d06973a09e5af786bd9b4dba4aa3
moatassim <moatassimbarakat@gmail.com>
2023-02-01 21:31:28 +0100

    databse changes

--- Routes/Auth.js
+++ Routes/Auth.js
@@ -1,11 +1,27 @@
 const express = require('express');
 const router = express.Router();
-const { loginUser, registerUser } = require('../Controllers/userController');
+const {
+  loginUser,
+  registerUser,
+  updateTestArrived,
+  updateTestScore,
+  updateAttestation,
+  updateTestStatus,
+} = require('../Controllers/userController');
 
 //register
 router.post('/register', registerUser);
 
 //Login
 router.post('/login', loginUser);
+
+// change test status
+router.post('/testChange', updateTestArrived);
+
+router.post('/testUpdateScore', updateTestScore);
+
+router.post('/gotAttestation', updateAttestation);
+
+router.post('/testStatus', updateTestStatus);
 
 module.exports = router;
